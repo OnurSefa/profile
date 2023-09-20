99e9c059c6cfadb0223e9974fd27f1401bd9906c
OnurSefa <onursefa_ozcibik@hotmail.com>
2023-09-20 20:37:50 +0300

    Project description update

--- src/Projects.js
+++ src/Projects.js
@@ -8,7 +8,7 @@ class Projects extends React.Component{
             projects: [
                 {
                     "title": "Generating Art using Generative Adversarial Networks",
-                    "description": "This project is my graduation project and we were group of 2 undergraduate students. I researched several vision methodologies and implemented a GAN to produce art images more purposefully using VQGAN+CLIP.",
+                    "description": "This project was my graduation project that lasted for two semesters. I have worked with my colleague. We used VQGAN and CLIP models to achieve good results. I have reimplemented the loss function to locate different text prompts to different locations on the generated images.",
                     "redirect": "/projects/generatingart",
                     "image": "https://user-images.githubusercontent.com/12373950/202875753-61866f25-7cfa-45c1-b1db-3a6545b8379f.png"
                 },
@@ -20,37 +20,37 @@ class Projects extends React.Component{
                 // },
                 {
                     "title": "Tax Office ID Extraction from Expense Documents",
-                    "description": "Tax Id defines the related tax office, it is crucial to extract this value correctly. I have used autoencoders, RNN, attention, and candidate embedding mechanism.",
+                    "description": "It is crucial to extract the Tax ID values from expense documents because they define the related tax offices. I used the autoencoder, RNN, and attention mechanism.",
                     "redirect": "/projects/taxid",
                     "image": "https://user-images.githubusercontent.com/12373950/202869526-f48e8411-f27b-4a93-b72a-44508d19133c.png"
                 },   
                 {
                     "title": "Open Domain End-to-End Question Answering System built on BERT",
-                    "description": "The project includes document retriever and document reader modules to detect correct paragraphs and then detect correct phrases according to question sentence.",
+                    "description": "The project includes a document retriever and document reader modules to answer questions using its open domain. The retriever selects the correct paragraphs and then the reader extracts the correct answers from selected sentences.",
                     "redirect": "/projects/questionanswer",
                     "image": "https://user-images.githubusercontent.com/12373950/202878698-f2145c0f-7b53-435d-8603-5250ec9330c9.png"
                 },
                 {
                     "title": "Data Visualization using PCA and t-SNE",
-                    "description": "Dimensionality reduction techniques are used on MNIST dataset to view how to visualize multidimensional data on 2D planes. Also, implementation of reconstruction methods of dimensionally reduced images.",
+                    "description": "I used dimensionality reduction techniques on the MNIST dataset to compare the results. Visualization of multidimensional data on a 2D plane is not an easy task but the mentioned methods achieve good results. ",
                     "redirect": "/projects/pca",
                     "image": "https://user-images.githubusercontent.com/12373950/202245488-d3e69941-4c52-44d3-98e4-cdc7478bae65.png"
                 },
                 {
                     "title": "Detection of Expense Documents' Payment Type",
-                    "description": "Detecting if the payment type is cash or credit card is essential for some companies.",
+                    "description": "The payment method of an expense document is crucial information for companies that offer company credit cards for their employees. The project aims to detect the payment type using photographs of expense documents.",
                     "redirect": "/projects/paymenttype",
                     "image": "https://user-images.githubusercontent.com/12373950/202870359-1f672a85-319f-4103-ba5e-82c7875592c5.png"
                 },             
                 {
                     "title": "Credit Card Number Extraction from Expense Documents",
-                    "description": "Repayment of some invoices can done by the credit card information. Finding this information accuratly helps companies.",
+                    "description": "Companies should pay the expenses of the employees only if the employee does not pay the bill with a company credit card. The project aims to detect the 4 last digits of the used credit card using photographs of expense documents.",
                     "redirect": "/projects/creditcard",
                     "image": "https://user-images.githubusercontent.com/12373950/202870647-3893771d-c014-4692-b72a-935cdda328a4.png"
                 },
                 {
                     "title": "Inspectation of Outlier Detection Methodologies",
-                    "description": "This project aims to display main differences betweeen two outlier detection methodologoies: Isolation Forest Model and Local Outlier Factor. It includes implementations of these two methods and comparisons.",
+                    "description": "This project aims to display the main differences between two outlier detection methods: the Isolation Forest Model and the Local Outlier Factor. It also tries to give intuition about both methods.",
                     "redirect": "/projects/outlier",
                     "image": "https://user-images.githubusercontent.com/12373950/202242561-d6078a8b-5c2e-4f5f-84a9-c0d38c2fd6e7.png"
                 },
@@ -62,7 +62,7 @@ class Projects extends React.Component{
                 // },
                 {
                     "title": "Expense Document Classification",
-                    "description": "There are several invoice types and detection of them highly affects posterior models.",
+                    "description": "There are several invoice types and detection of them highly affects posterior operations.",
                     "redirect": "/projects/invoicetype",
                     "image": "https://user-images.githubusercontent.com/12373950/202871487-90a00df6-0dd0-49b4-a8d0-a436955a43ee.png"
                 },
@@ -72,12 +72,12 @@ class Projects extends React.Component{
                 //     "redirect": "/projects/variableminimization",
                 //     "image": "https://user-images.githubusercontent.com/12373950/202872372-a525dc53-7129-49d2-84e8-3bcf368a8ed7.png"
                 // },
-                {
-                    "title": "Global Maxima Point Detector",
-                    "description": "Global maxima point detection methods and examples are investigated and implemented.",
-                    "redirect": "/projects/globalmaxima",
-                    "image": "https://user-images.githubusercontent.com/12373950/202873622-d7b051aa-aedb-4fff-a10e-8118b89152c7.png"
-                },
+                // {
+                //     "title": "Global Maxima Point Detector",
+                //     "description": "Global maxima point detection methods and examples are investigated and implemented.",
+                //     "redirect": "/projects/globalmaxima",
+                //     "image": "https://user-images.githubusercontent.com/12373950/202873622-d7b051aa-aedb-4fff-a10e-8118b89152c7.png"
+                // },
                 // {
                 //     "title": "Decision Tree",
                 //     "description": "I have implemented a decision tree algorithm that classifies points lie on a plane. Aim of the project was to visualize results of this algorithm in different stiuations and show when this algorithm is applicable and when it is not.",
@@ -203,10 +203,10 @@ class Projects extends React.Component{
         return <div className='projectsHolder'>
             <div className='projectsSpace'></div>
         <div className="projects">
-            <div className="projectsText">
+            {/* <div className="projectsText">
             &emsp;There are some of my projects. I upload them when I find an opportunity, so not all my projects are listed here. Also, most of them do not include related
             code parts. To reach them, or ask details about the projects, you can mail me.
-            </div>
+            </div> */}
             <div className='projectSections'>
                 {
                     projects.map((key)=>this.makeProject(key))
